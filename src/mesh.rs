59d950082a7bb8057a70226951ce6b907c7e17ac
Maciej Augustyniak <maciej@magmast.dev>
2024-08-15 13:04:54 +0200

    refactor: set hit front_face at creation time

--- src/mesh.rs
+++ src/mesh.rs
@@ -11,29 +11,23 @@ pub trait Mesh {
 pub struct Hit {
     pub point: Vec3,
     pub normal: Vec3,
-    pub t: f32,
+    pub distance: f32,
     pub front_face: bool,
     pub material: Arc<dyn Material>,
 }
 
 impl Hit {
-    pub fn new(point: Vec3, normal: Vec3, t: f32, material: Arc<dyn Material>) -> Self {
+    pub fn new(ray: &Ray3d, distance: f32, normal: Vec3, material: Arc<dyn Material>) -> Self {
+        let front_face = ray.direction.dot(normal) < 0.;
+        let point = ray.get_point(distance);
+
         Self {
             point,
-            normal,
-            t,
+            normal: if front_face { normal } else { -normal },
+            distance,
             front_face: true,
             material,
         }
-    }
-
-    pub fn set_face_normal(&mut self, ray: &Ray3d, outward_normal: Vec3) {
-        self.front_face = ray.direction.dot(outward_normal) < 0.;
-        self.normal = if self.front_face {
-            outward_normal.clone()
-        } else {
-            -outward_normal
-        };
     }
 }
 
@@ -54,7 +48,10 @@ impl Mesh for World {
         let mut current_hit: Option<Hit> = None;
 
         for mesh in &self.0 {
-            let tmax = current_hit.as_ref().map(|hit| hit.t).unwrap_or(ray_t.end);
+            let tmax = current_hit
+                .as_ref()
+                .map(|hit| hit.distance)
+                .unwrap_or(ray_t.end);
             let t = ray_t.start..tmax;
 
             if let Some(hit) = mesh.hit(ray, t) {
@@ -106,10 +103,7 @@ impl Mesh for Sphere {
         }
 
         let point = ray.get_point(root);
-        let outward_normal = (point - self.center) / self.radius;
-        let mut hit = Hit::new(point, outward_normal, root, self.material.clone());
-        hit.set_face_normal(ray, outward_normal);
-
-        Some(hit)
+        let normal = (point - self.center) / self.radius;
+        Some(Hit::new(ray, root, normal, self.material.clone()))
     }
 }
